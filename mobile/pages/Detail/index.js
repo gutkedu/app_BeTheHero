cca614f3f76583ae1e2c66ad444a47eabe0729fc
gutkedu <eduardo.pedogutkoski@gmail.com>
2020-03-27 23:27:55 -0300

    Mobile da aplicação

---
+++ mobile/pages/Detail/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View } from "react-native";
+
+import logoImg from "../../assets/logo.png";
+
+import styles from "./styles";
+
+export default function Detail() {
+  return (
+    <View>
+        :D
+    </View>
+  );
+}
+
